776b7195e345b2986e6daf67db5610fd6abe8b82
Dharmik Patel <dharmik.rising@gmail.com>
2026-01-07 13:29:58 +0530

    details Wroking on it

---
+++ Fixmycar UAE/Controller/Tabbar/History/SubVC/BookingDetails/View/BookingDetailsVC.swift
@@ -0,0 +1,18 @@
+//
+//  BookingDetailsVC.swift
+//  Fixmycar UAE
+//
+//  Created by Ankit on 07/01/26.
+//
+
+import UIKit
+
+class BookingDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
