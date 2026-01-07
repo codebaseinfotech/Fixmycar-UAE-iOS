780b23b3dade1f02e1a193059cd47de83dfe3ef5
Jenil iOS <jenil.codebase@gmail.com>
2026-01-07 18:31:07 +0530

    BookingDetails outlet done

--- Fixmycar UAE/Controller/Tabbar/History/SubVC/BookingDetails/View/BookingDetailsVC.swift
+++ Fixmycar UAE/Controller/Tabbar/History/SubVC/BookingDetails/View/BookingDetailsVC.swift
@@ -10,6 +10,31 @@ import UIKit
 class BookingDetailsVC: UIViewController {
 
     @IBOutlet weak var lblInvoiceName: UILabel!
+    @IBOutlet weak var imgRecovery: UIImageView!
+    @IBOutlet weak var lblServiceTitle: UILabel!
+    @IBOutlet weak var lblTime: UILabel!
+    @IBOutlet weak var viewStatus: UIView!
+    @IBOutlet weak var lblStatus: UILabel!
+    @IBOutlet weak var lblPickup: UILabel!
+    @IBOutlet weak var lblDrop: UILabel!
+    @IBOutlet weak var lblGulfside: UILabel!
+    @IBOutlet weak var lblGulfYear: UILabel!
+    @IBOutlet weak var viewGulfsideMain: UIView!
+    @IBOutlet weak var imgUser: UIImageView!
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet weak var lblRating: UILabel!
+    @IBOutlet weak var lblChat: UILabel!
+    @IBOutlet weak var lblVehicleTypeTitle: UILabel!
+    @IBOutlet weak var lblVehicleNumTitle: UILabel!
+    @IBOutlet weak var lblVehicleType: UILabel!
+    @IBOutlet weak var lblVehicleNum: UILabel!
+    @IBOutlet weak var lblDescription: UILabel!
+    @IBOutlet weak var lblAmount: UILabel!
+    @IBOutlet weak var lblBaseFare: UILabel!
+    @IBOutlet weak var lblDiscount: UILabel!
+    @IBOutlet weak var lblPlatformFee: UILabel!
+    @IBOutlet weak var lblTax: UILabel!
+    @IBOutlet weak var lblTotalAmount: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
